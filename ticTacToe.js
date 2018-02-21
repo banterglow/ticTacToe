d509eb60182d0a813e8eeab0305ad28a6dcd6dd0
wongal5 <a.wong9970@gmail.com>
2018-02-20 17:14:56 -0800

    fixed board formatting

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,6 +1,7 @@
 class TicTacToe {
   constructor() {
-    this.board = [['', '|', '', '|', ''], ['------------------'], ['', '|', '', '|', ''], ['------------------'], ['', '|', '', '|', '']];
+    this.emptyBoard = [['   ', '|', '   ', '|', '   '], ['------------'], ['   ', '|', '   ', '|', '   '], ['------------'], ['   ', '|', '   ', '|', '   ']];
+    this.board = this.emptyBoard;
     this.players = ['Player Two', 'Player One'];
     this.moves = { 'top': 0, 'middle': 2, 'bottom': 4, 'left': 0, 'right': 4 };
     this.turnNumber = 1;
@@ -10,7 +11,7 @@ class TicTacToe {
   checkWin(sym) {
     // check horizontal lines
     for (var h = 0; h <= 4; h+=2) {
-      if (this.board[h].join('') === `${sym}|${sym}|${sym}`) {
+      if (this.board[h].join('') === ` ${sym} | ${sym} | ${sym} `) {
         return true;
       }
     }
@@ -21,14 +22,14 @@ class TicTacToe {
       for (var a = 0; a <= 4; a+=2) {
         vertical.push(this.board[a][v]);
       }
-      if (vertical.join('') === `${sym}${sym}${sym}`) {
+      if (vertical.join('') === ` ${sym}  ${sym}  ${sym} `) {
         return true;
       }
     }
     // check diagonal lines
     let topLeft = [this.board[0][0], this.board[2][2], this.board[4][4]];
     let topRight = [this.board[0][4], this.board[2][2], this.board[4][0]];
-    if (topLeft.join('') === `${sym}${sym}${sym}` || topRight.join('') === `${sym}${sym}${sym}`) {
+    if (topLeft.join('') === ` ${sym}  ${sym}  ${sym} ` || topRight.join('') === ` ${sym}  ${sym}  ${sym} `) {
       return true;
     }
 
@@ -36,8 +37,8 @@ class TicTacToe {
   }
 
   endGame(playerName) {
-    console.log(this.board);
-    console.log(`${playerName} wins!`)
+    this.printBoard();
+    console.log(`\n${playerName} wins!`)
     this.resetBoard();
   }
 
@@ -45,14 +46,15 @@ class TicTacToe {
     let [ vert, horiz ] = location.split(' ');
     let playerTurn = this.players[this.turnNumber % 2];
     let playerSymbol = playerTurn === 'Player One' ? 'O' : 'X';
-    if (!this.board[this.moves[vert]][this.moves[horiz]]) {
-      this.board[this.moves[vert]][this.moves[horiz]] = playerSymbol;
+    if (!this.board[this.moves[vert]][this.moves[horiz]].trim()) {
+      this.board[this.moves[vert]][this.moves[horiz]] = ` ${playerSymbol} `;
       this.turnNumber++;
       this.checkWin(playerSymbol) ? this.endGame(playerTurn) : this.promptMove();
     } else {
       this.tryAgainMessage();
       this.promptMove();
     }
+    return '';
   }
 
   tryAgainMessage() {
@@ -61,15 +63,26 @@ class TicTacToe {
 
   promptMove() {
     let playerTurn = this.players[this.turnNumber%2];
-    console.log(this.board);
-    console.log(`Turn number ${this.turnNumber}. ${playerTurn}'s turn! Use game.move('location') to issue your next move. Ex. game.move('middle right')`);
+    let playerSymbol = playerTurn === 'Player One' ? 'O' : 'X';
+    this.printBoard();
+    console.log(`
+      ${playerTurn}'s turn! (${playerSymbol}) \n
+      Turn number ${ this.turnNumber }. \n
+      Use game.move('location') to issue your next move. Ex. game.move('middle right')
+    `);
+    return '';
+  }
+
+  printBoard() {
+    let stringBoard = this.board.map(row => row.join(''));
+    console.log(stringBoard.join('\n'));
   }
 
   resetBoard() {
-    this.board = [['', '|', '', '|', ''], ['-', '-', '-'], ['', '|', '', '|', ''], ['-', '-', '-'], ['', '|', '', '|', '']];
+    this.board = this.emptyBoard;
     this.turnNumber = 1;
   }
 
 }
 
-module.exports = TicTacToe;
+module.exports = new TicTacToe();
